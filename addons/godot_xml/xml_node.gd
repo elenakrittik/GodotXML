ff90378b58808c44e1d10c745a43ae7ea2fd76c6
lena <77104725+elenakrittik@users.noreply.github.com>
2023-09-19 21:53:53 +0300

    feat: Dotted child access (#3)

--- addons/godot_xml/xml_node.gd
+++ addons/godot_xml/xml_node.gd
@@ -16,6 +16,8 @@ var standalone: bool = false
 ## XML node's children.
 var children: Array[XMLNode] = []
 
+var _node_props = null  # Array[String]
+
 func _to_string():
 	return "<XMLNode name=%s attributes=%s content=%s standalone=%s children=%s>" % [
 		name,
@@ -24,3 +26,48 @@ func _to_string():
 		str(standalone),
 		"[...]" if len(children) > 0 else "[]"
 	]
+
+
+# Dotted access via GDScript
+func _get(property: StringName):
+	if _node_props == null:
+		_initialize_node_properties()
+
+	if property not in ["name", "attributes", "content", "standalone", "children"] and property in _node_props:
+		for child in children:
+			if child.name == property:
+				return child
+
+
+# Dotted access via editor
+func _get_property_list():
+	var props = []
+
+	if _node_props == null:
+		_initialize_node_properties()
+	
+	for child_name in _node_props:
+		var child = _get(child_name)
+
+		props.append({
+			"name": child_name,
+			"type": TYPE_OBJECT,
+			"class_name": "XMLNode",
+			"usage": PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_NONE,
+			"hint_string": "",
+		})
+	
+	return props
+
+
+func _initialize_node_properties():
+	var names_to_nodes = {}
+
+	for child in children:
+		if not child.name in names_to_nodes.keys():
+			names_to_nodes[child.name] = child
+		else:
+			names_to_nodes.erase(child.name)
+
+	_node_props = names_to_nodes.keys()
