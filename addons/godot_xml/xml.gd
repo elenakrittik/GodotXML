c7e9fb37a170d3faf35b0a71677eb67a25b5750e
lena <77104725+elenakrittik@users.noreply.github.com>
2024-03-13 19:01:07 +0300

    fix: Only remove trailing blankets (#22)

--- addons/godot_xml/xml.gd
+++ addons/godot_xml/xml.gd
@@ -207,10 +207,23 @@ static func _get_attributes(parser: XMLParser) -> Dictionary:
 static func _cleanup_double_blankets(xml: PackedByteArray) -> PackedByteArray:
     # XMLParser is again "incorrect" and duplicates nodes due to double blank escapes
     # https://github.com/godotengine/godot/issues/81896#issuecomment-1731320027
-    var cleaned := PackedByteArray()
 
-    for byte in xml:
-        if byte not in [9, 10, 13]: # [\t, \n, \r]
-            cleaned.append(byte)
+    var rm_count := 0 # How much elements (blankets) to remove from the source
+    var idx := xml.size() - 1
 
-    return cleaned
+    # Iterate in reverse order. This matters for perf because otherwise we
+    # would need to do a double .reverse() and remove elements from the start
+    # of the array, both of which are quite expensive
+    while idx >= 0:
+        if xml[idx] in [9, 10, 13]: # [\t, \n, \r]
+            rm_count += 1
+            idx -= 1
+        else:
+            break
+
+    # Remove blankets
+    while rm_count > 0:
+        xml.remove_at(xml.size() - 1)
+        rm_count -= 1
+
+    return xml
